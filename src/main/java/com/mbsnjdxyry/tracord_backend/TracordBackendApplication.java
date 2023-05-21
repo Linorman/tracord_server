a4bbfedc1454aed1660eb0f94970bf79f3b459be
Linorman <3033797357@qq.com>
2023-05-21 09:47:04 +0800

    initial push

---
+++ src/main/java/com/mbsnjdxyry/tracord_backend/TracordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mbsnjdxyry.tracord_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracordBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbsnjdxyry/tracord_backend/TracordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsnjdxyry.tracord_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
